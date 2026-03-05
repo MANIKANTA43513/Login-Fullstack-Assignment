8527e9eeb6aa6be844a347cf080e44a9bb0ae0be
Manikanta <manikandula456@gmail.com>
2026-03-05 09:47:03 +0530

    updated backend url

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -22,7 +22,7 @@ function Login() {
     setError("");
 
     try {
-      const res = await axios.post("http://localhost:5000/login", {
+      const res = await axios.post("https://login-fullstack-assignment.onrender.com/", {
         username,
         password
       });
